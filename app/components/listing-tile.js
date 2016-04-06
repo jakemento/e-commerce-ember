3b9b9525bb071affc1788f3b654ede9b856be29a
jake steinberg <jakesteinberg@gmail.com>
2016-04-06 15:21:51 -0700

    add computed property for combining listing name with price

--- app/components/listing-tile.js
+++ app/components/listing-tile.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  namePrice: Ember.computed('listing.name', 'listing.price', function() {
+    return this.get('listing.name') + '-' + '$' + this.get('listing.price');
+  }),
+
   actions: {
     delete(listing) {
       if(confirm('Are you sure you want to delete this listing?')) {
